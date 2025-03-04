ccb6cf0d9db335156ccbe67726713a999d55ac34
Anthony <kojiro5656i@gmail.com>
2025-03-04 13:21:17 +0100

    Mise en place du Header

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.scss";
+import logoheader from "../../assets/images/logokasa.png";
+import { Link, Links } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <NavLink to="/">
+        <img src={logoheader} alt="Logo Kasa" className="header__logo" />
+      </NavLink>
+
+      <nav className="header__navbarre">
+        <ul className="header__navbarre__ul">
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/">A Propos</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
