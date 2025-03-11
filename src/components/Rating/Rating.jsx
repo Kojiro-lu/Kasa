14cc268939eb554a79f6ad3ae90b8603681fcd65
Anthony <kojiro5656i@gmail.com>
2025-03-11 17:46:02 +0100

    réorganisation du css pour les components profile/rating/tag/titleandlocation

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -3,7 +3,23 @@ import OrangeStar from "../../assets/images/starorange.png";
 import GreyStar from "../../assets/images/stargrey.png";
 
 function Rating({ rating }) {
-  return <div className="rating-container"></div>;
+  const ratingNumber = parseInt(rating, 10);
+  const stars = [];
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= ratingNumber) {
+      stars.push(OrangeStar);
+    } else {
+      stars.push(GreyStar);
+    }
+  }
+  return (
+    <div className="rating-container">
+      {stars.map((star) => (
+        <img src={star} alt="Star" />
+      ))}
+    </div>
+  );
 }
 
 export default Rating;
--- src/pages/Logements/FicheLogement.jsx
+++ src/pages/Logements/FicheLogement.jsx
@@ -5,23 +5,32 @@ import Carrousel from "../../components/Caroussel/Carrousel";
 import DisplayTitleAndLocation from "../../components/TitleAndLocation/TitleAndLocation";
 import Profile from "../../components/Profile/Profile";
 import Tags from "../../components/Tags/Tags";
+import Rating from "../../components/Rating/Rating";
+import Collapse from "../../components/Collapse/Collapse";
 
 function FicheLogement() {
   const { id } = useParams(); // récupèration de l'ID dans l'url
   const logement = LogementsData.find((logement) => logement.id === id); // on recherche le logement qui correspond
 
   return (
-    <div className="Logement-Container">
+    <div className="logement-container">
       <Carrousel images={logement?.pictures} />
-      <div className="tile-and-profil-container">
-        <DisplayTitleAndLocation
-          title={logement.title}
-          location={logement.location}
-        />
-        <Profile name={logement.host.name} picture={logement.host.picture} />
+      <div className="middle-container">
+        <div className="tile-and-tags-container">
+          <DisplayTitleAndLocation
+            title={logement.title}
+            location={logement.location}
+          />
+          <Tags tags={logement.tags} />
+        </div>
+        <div className="profile-and-rate-container">
+          <Profile name={logement.host.name} picture={logement.host.picture} />
+          <Rating rating={logement.rating} />
+        </div>
       </div>
-      <div className="tags-and-rate-container">
-        <Tags tags={logement.tags} />
+      <div className="logement-collapse-container">
+        <Collapse title="Description" content={logement.description} />
+        <Collapse title="Equipements" content={logement.equipments} />
       </div>
     </div>
   );
