4226e24538196a2ff2f6935ed3e7394cdd36712b
Anthony <kojiro5656i@gmail.com>
2025-03-13 11:02:49 +0100

    Finalisation du responsive de la page erreur

--- src/pages/Erreur/Erreur.jsx
+++ src/pages/Erreur/Erreur.jsx
@@ -5,7 +5,10 @@ function Erreur() {
   return (
     <div className="erreur-container">
       <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <p>
+        Oups! La page que
+        <span>vous demandez n'existe pas.</span>
+      </p>
       <Link to="/" className="erreur-container__link-home-return">
         Retour à la page d'accueil
       </Link>
