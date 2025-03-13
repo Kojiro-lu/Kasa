3146ebb78414da6e1376e9cb93bbfb8dc0099fb7
Anthony <kojiro5656i@gmail.com>
2025-03-13 10:48:25 +0100

    mise en place du responsive pour le fichier erreur + modification du fichier rating.jsx, utilisation de la méthode map plutôt qu'une boucle for pour l'affichage des étoiles

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -3,20 +3,10 @@ import OrangeStar from "../../assets/images/starorange.png";
 import GreyStar from "../../assets/images/stargrey.png";
 
 function Rating({ rating }) {
-  const ratingNumber = parseInt(rating, 10);
-  const stars = [];
-
-  for (let i = 1; i <= 5; i++) {
-    if (i <= ratingNumber) {
-      stars.push(OrangeStar);
-    } else {
-      stars.push(GreyStar);
-    }
-  }
   return (
     <div className="rating-container">
-      {stars.map((star) => (
-        <img src={star} alt="Star" />
+      {[...Array(5)].map((_, i) => (
+        <img key={i} src={i < rating ? OrangeStar : GreyStar} alt="Star" />
       ))}
     </div>
   );
